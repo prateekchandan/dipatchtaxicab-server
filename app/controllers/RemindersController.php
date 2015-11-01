3c285c6183a05498eae436f9d04d03cab4a27b81
Prateek Chandan <prateekchandan5545@gmail.com>
2015-11-01 23:53:57 +0530

    Mail template change

--- app/controllers/RemindersController.php
+++ app/controllers/RemindersController.php
@@ -57,20 +57,20 @@ class RemindersController extends Controller {
 			'email', 'password', 'password_confirmation', 'token'
 		);
 
-		$passwordVal = new PasswordBroker();
-		$passwordVal->validator(function($credentials)
+		Password::validator(function($credentials)
 		{
 		    return true;
 		});
 
-		$response =$passwordVal->reset($credentials, function($user, $password)
+		$response =Password::eset($credentials, function($user, $password)
 		{
 			$user->password = Hash::make($password);
 
 			$user->save();
 		});
 
-		switch ($response)
+		echo $response;
+		/*switch ($response)
 		{
 			case Password::INVALID_PASSWORD:
 			case Password::INVALID_TOKEN:
@@ -79,7 +79,7 @@ class RemindersController extends Controller {
 
 			case Password::PASSWORD_RESET:
 				return Redirect::to('/');
-		}
+		}*/
 	}
 
 }
