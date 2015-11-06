0a4849afe11b66e9046a586b95f89902a43ea822
Prateek Chandan <prateekchandan5545@gmail.com>
2015-11-06 12:04:58 +0530

    activation filter

--- app/views/login/activate.blade.php
+++ app/views/login/activate.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 @section('content')
 
-					<section style="padding:10px;margin-top:100px">
+					<section style="padding:10px;margin-top:100px;margin-bottom:300px;">
 						<div class="alert alert-danger alert-dismissible" role="alert" style="margin:0px">
 						  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 						  Your Email Id is not verified. Please visit your email to activate your account.
